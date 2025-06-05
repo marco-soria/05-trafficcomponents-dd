39d7554c0918507b8675b7b19de99ca41153597e
marco-soria <marco.soriaa7@gmail.com>
2025-06-05 16:31:04 -0500

    contentchild

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,14 @@
-import { Component, ViewEncapsulation, input } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  ViewEncapsulation,
+  contentChild,
+  inject,
+  input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,7 +17,27 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+  private control =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+    console.log(this.control());
+  }
 }
